7e9c02a59b41bbe19bbe23bd0c597b8c320c07d4
adbhut96 <100306370+adbhut96@users.noreply.github.com>
2024-09-07 23:37:04 +0530

    [Update] - CORSPOLICY and env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 require('dotenv').config();
 import express from 'express';
+import cors from 'cors';
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+  }));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with akki server!');
